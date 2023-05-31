c24ad7179b1954ea05d1cf54fda29ad2e52f7fa0
pouterr <poutergutorov@gmail.com>
2023-06-01 00:06:45 +0600

    implementing bfs method

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,2 +1,26 @@
-public class BreadthFirstSearch {
+import java.util.*;
+public class BreadthFirstSearch<V> extends Search<V> {
+    public List<V> bfs(Vertex<V> source, Vertex<V> destination) {
+        Map<Vertex<V>, Vertex<V>> parentMap = new HashMap<>();
+        Queue<Vertex<V>> queue = new LinkedList<>();
+        Set<Vertex<V>> visited = new HashSet<>();
+
+        queue.offer(source);
+        visited.add(source);
+
+        while (!queue.isEmpty()) {
+            Vertex<V> current = queue.poll();
+            if (current.equals(destination)) {
+                return getPath(source, destination, parentMap);
+            }
+            for (Vertex<V> neighbor : current.getAdjacentVertices().keySet()) {
+                if (!visited.contains(neighbor)) {
+                    queue.offer(neighbor);
+                    visited.add(neighbor);
+                    parentMap.put(neighbor, current);
+                }
+            }
+        }
+        return null;
+    }
 }
