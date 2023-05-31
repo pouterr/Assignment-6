99aa395a540754f56e7d91cb0a42f2213efe2660
pouterr <poutergutorov@gmail.com>
2023-06-01 00:19:26 +0600

    documentating code

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,5 +1,17 @@
 import java.util.*;
+/**
+ * This class implements the Breadth-First Search algorithm on a graph.
+ *
+ * @param <V> the type of data each Vertex in the graph holds
+ */
 public class BreadthFirstSearch<V> extends Search<V> {
+    /**
+     * Executes the Breadth-First Search algorithm from a source Vertex to a destination Vertex.
+     *
+     * @param source the starting Vertex
+     * @param destination the ending Vertex
+     * @return a List of Vertices from source to destination representing the shortest path
+     */
     public List<V> bfs(Vertex<V> source, Vertex<V> destination) {
         Map<Vertex<V>, Vertex<V>> parentMap = new HashMap<>();
         Queue<Vertex<V>> queue = new LinkedList<>();
