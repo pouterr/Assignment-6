acb9077a557a4d7c1b83148bbbee6cc45f2dd160
pouterr <poutergutorov@gmail.com>
2023-05-31 23:59:56 +0600

    implementing getPath method

--- src/Search.java
+++ src/Search.java
@@ -1,3 +1,15 @@
-public class Search {
-
+import java.util.*;
+public class Search<V> {
+    public List<V> getPath(Vertex<V> source, Vertex<V> destination, Map<Vertex<V>, Vertex<V>> parentMap) {
+        LinkedList<V> path = new LinkedList<>();
+        Vertex<V> current = destination;
+        while (current != null) {
+            path.addFirst(current.getData());
+            current = parentMap.get(current);
+        }
+        if (path.getFirst().equals(source.getData())) {
+            return path;
+        }
+        return null;
+    }
 }
