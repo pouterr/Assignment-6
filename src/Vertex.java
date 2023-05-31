525be459cef29e94d0dfb04eef062750c29e2531
pouterr <poutergutorov@gmail.com>
2023-05-31 23:51:57 +0600

    implementing constructor

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,2 +1,9 @@
-public class Vertex {
+import java.util.*;
+public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices = new HashMap<>();
+
+    public Vertex(V data) {
+        this.data = data;
+    }
 }
