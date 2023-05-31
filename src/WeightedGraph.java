131a8fcde973cd584d5d73f8fd29d3a652dfc723
pouterr <poutergutorov@gmail.com>
2023-05-31 23:57:43 +0600

    implementing addVertex and getVertices methods

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -1,2 +1,13 @@
+import java.util.*;
+
 public class WeightedGraph<V> {
+    private List<Vertex<V>> vertices = new ArrayList<>();
+
+    public void addVertex(Vertex<V> vertex) {
+        vertices.add(vertex);
+    }
+
+    public List<Vertex<V>> getVertices() {
+        return vertices;
+    }
 }
