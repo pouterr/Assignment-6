8d8f13e314317cad414736f23b6b3ada30a677a0
pouterr <poutergutorov@gmail.com>
2023-06-01 00:17:30 +0600

    documentating code

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -1,12 +1,27 @@
 import java.util.*;
-
+/**
+ * This class represents a WeightedGraph. It maintains a list of vertices in the graph.
+ *
+ * @param <V> the type of data each Vertex in the graph holds
+ */
 public class WeightedGraph<V> {
+    // List of vertices in the graph
     private List<Vertex<V>> vertices = new ArrayList<>();
 
+    /**
+     * Adds a Vertex to the graph.
+     *
+     * @param vertex the Vertex to be added to the graph
+     */
     public void addVertex(Vertex<V> vertex) {
         vertices.add(vertex);
     }
 
+    /**
+     * Returns the list of vertices in the graph.
+     *
+     * @return the list of vertices in the graph
+     */
     public List<Vertex<V>> getVertices() {
         return vertices;
     }
